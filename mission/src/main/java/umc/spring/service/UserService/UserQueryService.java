d310256bc26bf0da669148fa4184d23c1df3c05d
beomsuyoo <beomsuyoo@gmail.com>
2023-12-05 20:17:51 +0900

    FEAT: 가게에 리뷰 추가 API 구현

--- mission/src/main/java/umc/spring/service/UserService/UserQueryService.java
+++ mission/src/main/java/umc/spring/service/UserService/UserQueryService.java
@@ -1,4 +1,10 @@
 package umc.spring.service.UserService;
 
+import umc.spring.domain.RestaurantEntity;
+import umc.spring.domain.UserEntity;
+
+import java.util.Optional;
+
 public interface UserQueryService {
+    Optional<UserEntity> findUser(Long id);
 }
--- mission/src/main/java/umc/spring/service/UserService/UserQueryServiceImpl.java
+++ mission/src/main/java/umc/spring/service/UserService/UserQueryServiceImpl.java
@@ -1,4 +1,22 @@
 package umc.spring.service.UserService;
 
-public class UserQueryServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import umc.spring.domain.RestaurantEntity;
+import umc.spring.domain.UserEntity;
+import umc.spring.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserQueryServiceImpl implements UserQueryService{
+    private final UserRepository userRepository;
+    @Override
+    public Optional<UserEntity> findUser(Long id) {
+        return userRepository.findById(id);
+    }
+
+    ;
 }
